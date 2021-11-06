c793e16ba4771974f58de5ce0879229a0b0c7201
yashiki25 <ymk.gr.22@gmail.com>
2021-11-07 01:20:39 +0900

    add OAuthによる認証機能を実装

---
+++ chat/auth.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// 未認証
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		// 何らかのエラー
+		panic(err.Error())
+	} else {
+		// 成功
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+// サードパーティへのログイン処理
+// パスの形式: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗しました: ", provider, "-", err)
+		}
+
+		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatalln("GetBeginAuthURLの呼び出し中にエラーが発生しました: ", provider, "-", err)
+		}
+
+		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗しました: ", provider, "-", err)
+		}
+
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("認証を完了できませんでした: ", provider, "-", err)
+		}
+
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("ユーザーの取得に失敗しました: ", provider, "-", err)
+		}
+
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/",
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+}
--- chat/main.go
+++ chat/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"flag"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 )
@@ -28,13 +35,27 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("環境変数が読み込めませんでした: ", err)
+	}
+
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+
+	gomniauth.SetSecurityKey(os.Getenv("AUTH_SECURITY_KEY"))
+	gomniauth.WithProviders(
+		facebook.New("", "", "http://localhost:8080/auth/callback/facebook"),
+		google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET"), "http://localhost:8080/auth/callback/google"),
+		github.New("", "", "http://localhost:8080/auth/callback/github"),
+		)
 
 	r := newRoom()
 	//r.tracer = trace.New(os.Stdout)
 
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 
